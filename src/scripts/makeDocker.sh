1cb97893d325090e599b21ddfc8223a2e30e9ffc
eivaskevicius <e.ivaskevicius@iplabs.de>
2018-03-02 14:42:02 +0100

    add make docker

---
+++ src/scripts/makeDocker.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+set -e -u -x
+
+# git clone resource-sbalpi-dev snapshot
+# cp -r node_modules snapshot
+
+cp -r node_modules resource-sbalpi
+cd resource-sbalpi
+
+npm remove jest
+rm -r src/__test__
+
+npm pack
+
+ls -la
+cd ..
+git clone resource-sbalpi snapshot
+cp resource-sbalpi/sbalpi* snapshot
+cd snapshot
+ls -la
+git config --global user.email "e.ivaskevicius@iplabs.de"
+git config --global user.name "eivaskevicius"
+npm version patch
+git add -A
+git status
+git commit -m "add snapshot"
+git pull

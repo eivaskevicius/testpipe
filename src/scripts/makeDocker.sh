8a4593dc9d0aef77a671d4bd376be95fc9c02206
eivaskevicius <e.ivaskevicius@iplabs.de>
2018-03-05 09:43:55 +0100

    comment docker commands

--- src/scripts/makeDocker.sh
+++ src/scripts/makeDocker.sh
@@ -21,18 +21,18 @@ service docker start
 service docker status
 sleep 5
 
-docker ps
-docker ps -a
-docker rm $(docker ps -qa --no-trunc --filter "status=exited")
+# docker ps
+# docker ps -a
+# docker rm $(docker ps -qa --no-trunc --filter "status=exited")
 
-docker volume rm $(docker volume ls -qf dangling=true)
-docker volume ls -qf dangling=true | xargs -r docker volume rm
+# docker volume rm $(docker volume ls -qf dangling=true)
+# docker volume ls -qf dangling=true | xargs -r docker volume rm
 
-docker images
-docker rmi $(docker images --filter "dangling=true" -q --no-trunc)
+# docker images
+# docker rmi $(docker images --filter "dangling=true" -q --no-trunc)
 
-docker images | grep "none"
-docker rmi $(docker images | grep "none" | awk '/ / { print $3 }')
+# docker images | grep "none"
+# docker rmi $(docker images | grep "none" | awk '/ / { print $3 }')
 
 docker build -t eivaskevicius/docker .
 docker save -o docker.tar eivaskevicius/docker
